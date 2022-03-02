9587f4febac24f669271dc6f8b83590552fa5ce8
Bavon101 <bavonfate@gmail.com>
2022-03-02 13:09:15 +0300

    Add Game class to mutate score's datat

---
+++ src/modules/game.js
@@ -0,0 +1,26 @@
+export default class Game {
+  gameScores = {};
+
+  gameName = 'The Walking Dead';
+
+  gameId = 'SfnXixPrHikLvDkF1aQm';
+
+  baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+  addGame = async (score) => {
+    const results = await fetch(`${this.baseUrl}games/${this.gameId}/scores/`, {
+      method: 'POST',
+      body: JSON.stringify(score),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    return results;
+  };
+
+  getScores = async () => {
+    const scores = await fetch(`${this.baseUrl}games/${this.gameId}/scores/`, { method: 'GET' });
+    this.gameScores = await scores.json();
+    return scores;
+  }
+}
